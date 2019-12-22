3a9d9d02e87ca31a0113087ce6b26f0d5bc26bc1
Henry Phan <h.phan06@gmail.com>
2019-12-22 21:52:34 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -17,9 +17,9 @@ class Song
     song 
   end
   
-  def self.new_by_name(name)
+  def self.new_by_name(title)
     song = self.create
-    song.name = name 
+    song.name = title
     song
   end
   
